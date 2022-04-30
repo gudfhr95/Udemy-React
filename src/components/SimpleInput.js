47a5fe6d9585e103ee699bdd073137b9b41d335e
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-04-30 23:29:13 +0900

    "was touched" State 처리하기

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,16 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enterdName, setEnteredName] = useState("");
-  const [enterednameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enterdNameTouched, setEnterdNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is valid!");
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -11,6 +18,8 @@ const SimpleInput = (props) => {
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
+
+    setEnterdNameTouched(true);
 
     if (enterdName.trim() === "") {
       setEnteredNameIsValid(false);
@@ -28,9 +37,11 @@ const SimpleInput = (props) => {
     setEnteredName("");
   };
 
-  const nameInputClasses = enterednameIsValid
-    ? "form-control"
-    : "form-control invalid";
+  const nameInputIsInvalid = !enteredNameIsValid && enterdNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -43,7 +54,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enterdName}
         />
-        {!enterednameIsValid && (
+        {nameInputIsInvalid && (
           <p className="error-text">Name must not be empty.</p>
         )}
       </div>
