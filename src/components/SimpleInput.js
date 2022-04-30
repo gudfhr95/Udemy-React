4c21bfbd479eea3c611b03d45976b4f3ea5745a6
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-04-30 23:50:37 +0900

    전체 양식 유효성 관리하기

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -7,6 +7,12 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enterdName.trim() !== "";
   const nameInputIsInvalid = !enteredNameIsValid && enterdNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
   };
@@ -51,7 +57,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
