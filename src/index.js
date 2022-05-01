f112a75594c5d40db9d76b1d8bffa0ea694c7500
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-01 23:29:42 +0900

    스토어 제공하기

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import store from "./store/index";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={StorageEvent}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
