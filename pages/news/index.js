2403eef32be5a66634c63404257d8bae0a8af53b
Hyungrok Ham <ghldnjsdlf@gmail.com>
2022-05-07 21:56:32 +0900

    페이지 간 연결하기

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 // our-domain.com/news
 
 function NewsPage() {
-  return <h1>The News Page</h1>;
+  return (
+    <Fragment>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Is A Great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
